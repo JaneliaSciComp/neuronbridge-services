7886a455d2fd56726ed3b4ccf13f9a589a42a01d
Konrad Rokicki <rokickik@janelia.hhmi.org>
2020-06-05 00:30:39 -0400

    added timeout and other fixes

--- search/src/main/nodejs/monitor.js
+++ search/src/main/nodejs/monitor.js
@@ -4,6 +4,7 @@ const moment = require('moment');
 
 const s3 = new AWS.S3();
 const DEBUG = false;
+const SEARCH_TIMEOUT_SECS = process.env.SEARCH_TIMEOUT_SECS;
 
 // From https://stackoverflow.com/questions/42394429/aws-sdk-s3-best-way-to-list-all-keys-with-listobjectsv2
 async function getAllKeys(params,  allKeys = []) {
@@ -43,6 +44,7 @@ async function putObject(bucket, key, data) {
         console.error('Error putting object', data, `to ${bucket}:${key}`, e);
         throw e;
     }
+    return `s3://${bucket}/${key}`
 }
 
 exports.isSearchDone = async (event, context) => {
@@ -85,32 +87,54 @@ exports.isSearchDone = async (event, context) => {
     if (completed) {
         console.log(`Search complete: ${numComplete}/${numPartitions}`);
 
-        // Calculate total search time
-        const now = new Date();
-        const endTime = moment(now.toISOString());
-        const elapsedSecs = endTime.diff(startTime, "s");
-        console.log(`Search took ${elapsedSecs} seconds`);
-
         // Combine all the results
         const allMatches = [];
         for(let i=0; i<numPartitions; i++) {
             const batchId = i.toString().padStart(4,"0");
             const outputKey = prefix+"/batch_"+batchId+".json";
-            const batch = await getObject(bucket, outputKey);
-            for(const match of batch) {
-                allMatches.push(match);
+            const batchResults = await getObject(bucket, outputKey);
+            for(var j=0; j<batchResults.length; j++) {
+                var maskMatches;
+                if (i==0) {
+                    maskMatches = [];
+                    allMatches.push(maskMatches);
+                }
+                else {
+                    maskMatches = allMatches[j];
+                }
+                for(const result of batchResults[j]) {
+                    maskMatches.push(result);
+                }
             }
         }
+        console.log(`Found ${allMatches.length} total matches`);
+
         // Sort the matches by descending score
-        const sortedMatches = allMatches.sort((a, b) => b.score - a.score);
+        for(let i=0; i<allMatches.length; i++) {
+            allMatches[i] = allMatches[i].sort((a, b) => b.score - a.score);
+        }
+
         // Collate all matches to a file
-        await putObject(bucket, prefix+"/results.json", sortedMatches);
+        const outputUri = await putObject(bucket, prefix+"/results.json", allMatches);
+        console.log(`Saved matches to ${outputUri}`);
+
+        // Calculate total search time
+        const now = new Date();
+        const endTime = moment(now.toISOString());
+        const elapsedSecs = endTime.diff(startTime, "s");
+
+        if (elapsedSecs > SEARCH_TIMEOUT_SECS) {
+            throw new Error(`Search timed out after ${elapsedSecs} seconds`);
+        }
+
+        console.log(`Search took ${elapsedSecs} seconds`);
 
         // Return results which Step Functions will use to determine if this monitor should run again
         return {
             ...event,
             elapsedSecs: elapsedSecs,
             numPartitions: numPartitions,
+            numRemaining: 0,
             completed: true
         };
     }
