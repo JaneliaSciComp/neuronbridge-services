7a6c1345f474696f014575b7bc3763a51c4cc7b9
Cristian Goina <goinac@hhmi.org>
2020-07-30 16:19:08 -0400

    putitem in dynamo

--- search/src/main/nodejs/search_manager.js
+++ search/src/main/nodejs/search_manager.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const AWS = require("aws-sdk");
-const dynamo = new AWS.DynamoDB.DocumentClient();
 const { v1: uuidv1 } = require('uuid');
 const moment = require('moment');
 
 const SEARCH_TABLE = process.env.SEARCH_TABLE;
+
+AWS.config.update({
+    apiVersion: '2012-08-10'
+});
+
+const dynamoDB = new AWS.DynamoDB();
 
 const getArgs = (e) => { return e.source === 'graphql' ? e.arguments : e; }
 
@@ -31,28 +36,34 @@ const createSearch = async (searchParams) => {
     console.log("!!!! CREATE SEARCH ", searchParams);
     const searchId = uuidv1();
     const now = new Date();
-    const searchTimestamp = moment(now.toISOString());
+    const searchTimestamp = now.toISOString();
     const searchItem = {
+        id: {S: searchId},
+        __typename: {S: 'Search'},
+        step: {N: "0"},
+        createdOn: {S: searchTimestamp},
+        updatedOn: {S: searchTimestamp},
+        owner: {S: searchParams.owner},
+        identityId: {S: searchParams.identityId},
+        searchType: {S: searchParams.searchType},
+        searchDir: {S: searchParams.searchDir},
+        upload: {S: searchParams.upload},
+        algorithm: {S: searchParams.algorithm},
+        mimeType: {S: searchParams.mimeType},
+    };
+    const params = {
+        Item: searchItem,
+        TableName: `${SEARCH_TABLE}`
+    };
+    console.log("PutItem", params);
+    const newSearch = await dynamoDB.putItem(params).promise();
+    console.log("Created search", newSearch);
+    return {
         id: searchId,
         step: 0,
         createdOn: searchTimestamp,
         updatedOn: searchTimestamp,
         ...searchParams
-    }
-    const params = {
-        Key: {
-            id: searchId
-        },
-        Item: searchItem,
-        TableName: `${SEARCH_TABLE}`,
-        ReturnValues: 'ALL_NEW'
-    };
-
-    return await dynamo.put(params);
-
-    return {
-        id: "new",
-        algorithm: "max"
     };
 }
 
@@ -64,9 +75,8 @@ const updateSearch = (searchParams) => {
     console.log("!!!! UPDATE SEARCH ", searchParams);
 }
 
-exports.searchManager = async (event, context) => {
+exports.searchManager = async (event) => {
     console.log(event);
-    console.log("!!!!!!!", context);
 
     const action = event.action;
     const args = getArgs(event);
