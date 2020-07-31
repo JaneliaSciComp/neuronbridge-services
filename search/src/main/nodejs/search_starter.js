d72b3bd70be1566bdd039ed23bf3ba6b4aaa8c0d
Cristian Goina <goinac@hhmi.org>
2020-07-31 19:13:00 -0400

    search retrieve works now

--- search/src/main/nodejs/search_starter.js
+++ search/src/main/nodejs/search_starter.js
@@ -2,25 +2,46 @@
 
 const AWS = require("aws-sdk");
 
+const SEARCH_TABLE = process.env.SEARCH_TABLE;
+
 AWS.config.update({
     apiVersion: '2012-08-10'
 });
+
+const dynamoDocClient = new AWS.DynamoDB.DocumentClient();
 
 const startColorDepthSearch = async (searchParams) => {
     console.log('Start ColorDepthSearch', searchParams);
 }
 
-const getNewRecords = (e) => {
-    return e.Records
+const getSearch = async (id) => {
+    console.log('Get Search', id);
+    const params = {
+        TableName: `${SEARCH_TABLE}`,
+        Key: {
+            'id': id
+        }
+    };
+    console.log('GetItem', params);
+    const searchData = await dynamoDocClient.get(params).promise();
+    console.log('Found search', searchData);
+    return searchData ? searchData.Item : null;
+}
+
+const getNewRecords = async (e) => {
+    const newRecordsPromises = await e.Records
         .filter(r => r.eventName === 'INSERT')
-        .map(r => r.dynamodb);
+        .map(r => r.dynamodb)
+        .map(r => r.Keys.id.S)
+        .map(async searchId => await getSearch(searchId));
+    return await Promise.all(newRecordsPromises);
 }
 
 exports.searchStarter = async (event) => {
     console.log(event);
-    getNewRecords(event)
-        .forEach(r => {
-            startColorDepthSearch(r);
-        });
+    const newRecords = await getNewRecords(event);
+    newRecords.forEach(r => {
+        startColorDepthSearch(r);
+    });
 };
 
