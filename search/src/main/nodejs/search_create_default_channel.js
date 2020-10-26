d4708c5c40b5cb431c1741c7becb64bac3b34825
Jody Clements <jody@neomorphic.co.uk>
2020-10-26 13:41:29 -0400

    Splits search_copy function into three functions
    This simplifies the logic of each function and also allows us to easily
    see which functions are being called the most.

---
+++ search/src/main/nodejs/search_create_default_channel.js
@@ -0,0 +1,100 @@
+"use strict";
+
+const Jimp = require("jimp");
+const {
+  getS3ContentWithRetry,
+  copyS3Content,
+  putS3Content,
+} = require("./utils");
+const { getSearchKey, getSearchMaskId } = require("./searchutils");
+const {
+  updateSearchMetadata,
+  getSearchMetadata,
+  ALIGNMENT_JOB_COMPLETED
+} = require("./awsappsyncutils");
+
+const searchBucket = process.env.SEARCH_BUCKET;
+const s3Retries = process.env.S3_RETRIES || 3;
+
+async function getSearchRecord(searchId) {
+  const searchMetadata = await getSearchMetadata(searchId);
+  return searchMetadata;
+}
+
+async function createDefaultChannel(searchData) {
+  const { id, upload, searchInputFolder, identityId, searchDir } = searchData;
+  // TODO: add a check for the image extension here. We should only be copying
+  // png, jpeg, bmp, tiff or gif images that are already aligned. The 3D stacks
+  // need to go through the aligner which will output the channels for masking
+
+  const fullSearchInputImage = `${searchInputFolder}/${upload}`;
+  // grab the image data
+  const imageContent = await getS3ContentWithRetry(
+    searchBucket,
+    fullSearchInputImage,
+    s3Retries
+  );
+
+  let sourceImage = fullSearchInputImage;
+  let channelName = upload.replace(/\.([^.]*)$/, "_1.$1");
+
+  const searchMetaData = {
+    id,
+    step: ALIGNMENT_JOB_COMPLETED
+  };
+
+  const pngMime = "image/png";
+
+  // if not a png, transform to png
+  if (/\.(tiff?|gif|jpe?g|bmp)$/.test(upload)) {
+    const pngExt = ".png";
+    const image = await Jimp.read(imageContent);
+    const imageBuffer = await image.getBufferAsync(pngMime);
+    const pngImageName = getSearchKey(fullSearchInputImage, pngExt);
+    sourceImage = pngImageName;
+    channelName = upload.replace(/\.([^.]*)$/, "_1.png");
+    await putS3Content(searchBucket, pngImageName, pngMime, imageBuffer);
+    searchMetaData.displayableMask = getSearchMaskId(pngImageName, pngExt);
+  }
+  // create new file in generatedMIPS directory as channel_1.png
+  const channelPath = `private/${identityId}/${searchDir}/generatedMIPS/${channelName}`;
+  await copyS3Content(
+    searchBucket,
+    `/${searchBucket}/${sourceImage}`,
+    channelPath
+  );
+
+  // create a thumbnail of the uploaded image
+  const thumbnailName = "upload_thumbnail.png";
+  const original = await Jimp.read(imageContent);
+  const thumbnail = original.scaleToFit(150, 70);
+  const thumbnailBuffer = await thumbnail.getBufferAsync(pngMime);
+  const thumbnailPath = `private/${identityId}/${searchDir}/${thumbnailName}`;
+  await putS3Content(searchBucket, thumbnailPath, pngMime, thumbnailBuffer);
+  searchMetaData.uploadThumbnail = thumbnailName;
+
+  await updateSearchMetadata(searchMetaData);
+  return { id };
+}
+
+exports.searchCreateDefaultChannel = async (event) => {
+  const returnObj = {
+    isBase64Encoded: false,
+    statusCode: 200,
+    body: JSON.stringify({})
+  };
+  let returnBody = {};
+
+  try {
+    const { searchId } = JSON.parse(event.body);
+      const searchData = await getSearchRecord(searchId);
+      returnBody = await createDefaultChannel(searchData);
+  } catch (error) {
+    returnObj.statusCode = 500;
+    returnBody.message = error.message;
+  }
+
+  returnObj.body = JSON.stringify(returnBody);
+
+  return returnObj;
+};
---
+++ search/src/main/nodejs/search_new_from_image.js
@@ -0,0 +1,96 @@
+"use strict";
+
+const { v1: uuidv1 } = require("uuid");
+const {
+  copyS3Content,
+} = require("./utils");
+const {
+  createSearchMetadata,
+  ALIGNMENT_JOB_COMPLETED
+} = require("./awsappsyncutils");
+
+const searchBucket = process.env.SEARCH_BUCKET;
+
+/*
+ *  Given an s3 image url and a users identity, this function will
+ *  copy that image into a new search record and set it up so that
+ *  the user can create a new mask and run a search.
+ */
+async function createNewSearchFromImage(image, event, identityId) {
+  // generate a new id for the search directory
+  const newSearchId = uuidv1();
+  // create new search directory
+  const newSearchFolder = `private/${identityId}/${newSearchId}`;
+  // convert image.imageURL to bucket location
+  const [, originalBucket, originalPath, originalImage] = image.imageURL.match(
+    /^.*s3.amazonaws.com\/([^/]*)(.*?)([^/]*)$/
+  );
+  // copy image to upload
+  await copyS3Content(
+    searchBucket,
+    `/${originalBucket}${originalPath}${originalImage}`,
+    `${newSearchFolder}/${originalImage}`
+  );
+
+  // convert image.thumbnailURL to new bucket location
+  const [, thumbnailBucket, thumbnailPath, thumbnailUpload, extension] = image.thumbnailURL.match(
+    /^.*s3.amazonaws.com\/([^/]*)(.*?)([^/]*?)([^.]*)$/
+  );
+
+  const newThumbnailName = `upload_thumbnail.${extension}`;
+
+  // copy image thumbnail to new bucket
+  await copyS3Content(
+    searchBucket,
+    `/${thumbnailBucket}${thumbnailPath}${thumbnailUpload}${extension}`,
+    `${newSearchFolder}/${newThumbnailName}`
+  );
+  //
+  // generate mip channel for this image
+  let channelName = originalImage.replace(/\.([^.]*)$/, "_1.$1");
+  const channelPath = `private/${identityId}/${newSearchId}/generatedMIPS/${channelName}`;
+  await copyS3Content(
+    searchBucket,
+    `/${originalBucket}${originalPath}${originalImage}`,
+    channelPath
+  );
+
+  // create new data object to store in dynamoDB
+  const ownerId = event.requestContext.authorizer.jwt.claims.sub;
+  // set step to mask selection
+  const newSearchData = {
+    step: ALIGNMENT_JOB_COMPLETED,
+    owner: ownerId,
+    identityId: identityId,
+    searchDir: newSearchId,
+    upload: originalImage,
+    simulateMIPGeneration: false,
+    uploadThumbnail: newThumbnailName
+  };
+  // save new data object- in dynamoDB
+  const newSearchMeta = await createSearchMetadata(newSearchData);
+
+  return { newSearchMeta };
+}
+
+exports.searchNewFromImage = async (event) => {
+  const returnObj = {
+    isBase64Encoded: false,
+    statusCode: 200,
+    body: JSON.stringify({})
+  };
+  let returnBody = {};
+
+  try {
+    // get the search id from the post body
+    const { image, identityId } = JSON.parse(event.body);
+    returnBody = await createNewSearchFromImage(image, event, identityId);
+  } catch (error) {
+    returnObj.statusCode = 500;
+    returnBody.message = error.message;
+  }
+
+  returnObj.body = JSON.stringify(returnBody);
+
+  return returnObj;
+};
