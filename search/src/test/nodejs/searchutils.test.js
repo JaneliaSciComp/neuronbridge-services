a2c9c0a4249dd34581c57f50b2b452327ca29d98
Cristian Goina <goinac@hhmi.org>
2020-10-28 10:27:36 -0400

    babel presets for the jest unit test

--- search/src/test/nodejs/searchutils.test.js
+++ search/src/test/nodejs/searchutils.test.js
@@ -1,18 +1,18 @@
-const searchutils = require('../../main/nodejs/searchutils');
+import { getSearchKey, getSearchMaskId, getIntermediateSearchResultsPrefix } from '../../main/nodejs/searchutils';
 
 test('getSearchKey', () => {
-  expect(searchutils.getSearchKey('/path', '.png')).toBe('/path.png');
-  expect(searchutils.getSearchKey('/path', '')).toBe('/path');
+  expect(getSearchKey('/path', '.png')).toBe('/path.png');
+  expect(getSearchKey('/path', '')).toBe('/path');
 });
 
 test('getSearchMaskId', () => {
-  expect(searchutils.getSearchMaskId('path.png')).toBe('path');
-  expect(searchutils.getSearchMaskId('folder/path.png')).toBe('path');
+  expect(getSearchMaskId('path.png')).toBe('path');
+  expect(getSearchMaskId('folder/path.png')).toBe('path');
 });
 
 test('getSearchSubFolder', () => {
-  expect(searchutils.getIntermediateSearchResultsPrefix('folder/path')).toBe('folder/results');
-  expect(searchutils.getIntermediateSearchResultsPrefix('folder/path.png')).toBe('folder/results');
-  expect(searchutils.getIntermediateSearchResultsPrefix('path')).toBe('results');
-  expect(searchutils.getIntermediateSearchResultsPrefix('path.png')).toBe('results');
+  expect(getIntermediateSearchResultsPrefix('folder/path')).toBe('folder/results');
+  expect(getIntermediateSearchResultsPrefix('folder/path.png')).toBe('folder/results');
+  expect(getIntermediateSearchResultsPrefix('path')).toBe('results');
+  expect(getIntermediateSearchResultsPrefix('path.png')).toBe('results');
 });
