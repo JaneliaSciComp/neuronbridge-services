e7a18846a778130cf7be77191bb87a5136d68765
Jody Clements <jody@neomorphic.co.uk>
2021-06-18 11:05:48 -0400

    Adds UserMigration_ForgotPassword flow.
    This adds code to the user migration lambda to deal with migration when
    the user has forgotten their password.

--- user_migration_lamda.js
+++ user_migration_lamda.js
@@ -1,11 +1,10 @@
 const AWS = require("aws-sdk");
-const db = new AWS.DynamoDB.DocumentClient();
+const isp = new AWS.CognitoIdentityServiceProvider();
 
 const OLD_CLIENT_ID = process.env.OLD_CLIENT_ID;
 const OLD_USER_POOL_ID = process.env.OLD_USER_POOL_ID;
 
 async function authenticateUser(username, password) {
-  const isp = new AWS.CognitoIdentityServiceProvider();
 
   // validate supplied username & password
   const resAuth = await isp
@@ -34,73 +33,39 @@ async function authenticateUser(username, password) {
     return undefined;
   }
 
-  console.log(resGet.UserAttributes);
-
   return {
     emailAddress: resGet.UserAttributes.find(e => e.Name === "email").Value,
     sub: resGet.UserAttributes.find(e => e.Name === "sub").Value
   };
 }
 
-//fetch data from original dynamodb table
-async function fetchData(ownerId) {
-  const params = {
-    TableName: "janelia-neuronbridge-search-table-prod",
-    FilterExpression: "#owner = :owner",
-    ExpressionAttributeValues: {
-      ":owner": ownerId
-    },
-    ExpressionAttributeNames: {
-      "#owner": "owner"
-    }
-  };
-
+async function lookupUser(username) {
   try {
-    const data = await db.scan(params).promise();
-    return data;
-  } catch (err) {
-    return err;
-  }
-}
-
-// save data to new dynamodb table
-async function saveNewItem(item) {
-  const params = {
-    TableName: "janelia-neuronbridge-search-table-dev",
-    Item: item
-  };
-
-  try {
-    await db.put(params).promise();
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
-}
-
-async function migrate_dynamodb(sub) {
-  const originalData = await fetchData(sub);
-
-  for (let record of originalData.Items) {
-    const updatedRecord = { ...record, migrated: true };
-    try {
-      await saveNewItem(updatedRecord);
-    } catch (err) {
-      console.log(err);
+    const params = {
+      UserPoolId: OLD_USER_POOL_ID,
+      Username: username,
+    };
+    const resGet = await isp.adminGetUser(params).promise();
+    if (resGet.code && resGet.message) {
+      return undefined;
     }
-    console.log("saved record to new table");
+    return {
+      emailAddress: resGet.UserAttributes.find(e => e.Name === "email").Value,
+      sub: resGet.UserAttributes.find(e => e.Name === "sub").Value
+    };
+  } catch (err) {
+      console.log(`lookupUser: error ${JSON.stringify(err)}`);
+    return undefined;
   }
 }
 
 exports.handler = async (event, context, callback) => {
   // TODO implement
   if (event.triggerSource === "UserMigration_Authentication") {
+    console.log(`migrating user: ${event.userName}`);
     const user = await authenticateUser(event.userName, event.request.password);
 
     if (user) {
-      console.log("migrating user");
-      // migrate dynamodb data
-      const result = await migrate_dynamodb(user.sub);
 
       console.log("adding user to new pool");
       // add user to new user pool
@@ -116,6 +81,21 @@ exports.handler = async (event, context, callback) => {
       // migrate s3 buckets
     } else {
       callback("Username & password entered was not correct");
+    }
+  } else if (event.triggerSource === "UserMigration_ForgotPassword") {
+    console.log(`migrating user: ${event.userName}`);
+    const user = await lookupUser(event.userName);
+    if (user) {
+      console.log(user);
+      event.response.userAttributes = {
+        email: user.emailAddress,
+        email_verified: "true",
+        "custom:migrated": "true"
+      };
+      event.response.messageAction = "SUPPRESS";
+      context.succeed(event);
+    } else {
+      callback("Username/client id combination not found.");
     }
   } else {
     // Return error to Amazon Cognito
